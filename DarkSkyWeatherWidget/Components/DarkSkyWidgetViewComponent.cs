6cabacb865829a1fc87ed907da7bf31607f69b0b
Çağatay Ekrem KAYA <cacalonga@hotmail.com>
2020-04-01 16:40:03 +0300

    First_commit

---
+++ DarkSkyWeatherWidget/Components/DarkSkyWidgetViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DarkSky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DarkSkyWeatherWidget.Components
+{
+	public class DarkSkyWidgetViewComponent: ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			OptionalParameters param = new OptionalParameters();
+			param.LanguageCode = "tr";
+			param.MeasurementUnits = "si";
+			var darkSky = new DarkSky.Services.DarkSkyService("KEY");
+			DarkSkyResponse forecast = await darkSky.GetForecast(40.1766256, 28.7582773, param);
+			return View("DarkSkyWidget", forecast);
+		}
+	}
+}
---
+++ DarkSkyWeatherWidget/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DarkSkyWeatherWidget
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+			services.AddSingleton(HtmlEncoder.Create(allowedRanges: new[] { UnicodeRanges.BasicLatin, UnicodeRanges.Latin1Supplement, UnicodeRanges.LatinExtendedA }));
+
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+
+		}
+	}
+}
